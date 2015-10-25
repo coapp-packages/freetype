2ffe8b83ae12d58d30734b33a47cbb7ccde1036e
Vincent Povirk <madewokherd@gmail.com>
2015-10-25 12:18:26 -0500

    Add support for VS2013 and VS2015.

--- COPKG/all.bat
+++ COPKG/all.bat
@@ -7,6 +7,30 @@ if "%1"=="noclean" (
 	shift)
 
 setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+call :build x64 Release v140
+call :build x64 Debug v140
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v140
+call :build Win32 Debug v140
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+call :build x64 Release v120
+call :build x64 Debug v120
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v120
+call :build Win32 Debug v120
+endlocal
+
+setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
 call :build x64 Release v110
 call :build x64 Debug v110
