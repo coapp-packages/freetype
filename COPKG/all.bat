332a8334524524ce28428d9501c9d9be9ad34e7a
Vincent Povirk <madewokherd@gmail.com>
2015-12-13 13:31:17 -0600

    Update build files for 2.6.2.

--- COPKG/all.bat
+++ COPKG/all.bat
@@ -7,49 +7,21 @@ if "%1"=="noclean" (
 	shift)
 
 setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 call :build x64 Release v140
 call :build x64 Debug v140
-endlocal
-
-setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 call :build Win32 Release v140
 call :build Win32 Debug v140
-endlocal
-
-setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 call :build x64 Release v120
 call :build x64 Debug v120
-endlocal
-
-setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 call :build Win32 Release v120
 call :build Win32 Debug v120
-endlocal
-
-setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
 call :build x64 Release v110
 call :build x64 Debug v110
-endlocal
-
-setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 call :build Win32 Release v110
 call :build Win32 Debug v110
-endlocal
-
-setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
 call :build x64 Release v100
 call :build x64 Debug v100
-endlocal
-
-setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 call :build Win32 Release v100
 call :build Win32 Debug v100
 endlocal
